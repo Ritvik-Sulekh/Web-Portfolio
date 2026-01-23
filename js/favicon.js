d6fbd6744b474dd8608267bf8ae7ddc76859955b
Ritvik <51816007+Ritvik-Sulekh@users.noreply.github.com>
2026-01-23 23:45:47 +0530

    Delete js directory

--- js/favicon.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  const link = document.createElement("link");
-  link.rel = "icon";
-  link.type = "image/png";
-  link.sizes = "512x512";
-  link.href = window.location.origin + "/assets/tab%20logo/letter-r.png"; 
-  document.head.appendChild(link);
-})();
