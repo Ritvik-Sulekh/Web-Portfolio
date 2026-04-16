f3efe8a8ac43c7937d1d4ebadf369bce28d7eff8
Ritvik <51816007+Ritvik-Sulekh@users.noreply.github.com>
2026-04-17 00:30:46 +0530

    Update pors.html.js

--- components/pors/pors.html.js
+++ components/pors/pors.html.js
@@ -28,8 +28,8 @@ function renderPors() {
           </div>
           <div class="pors__line"></div>
           <div class="pors__content">
-            <div class="pors__role">President</div>
-            <div class="pors__org">Marketing Club, IIM Udaipur</div>
+            <div class="pors__role">X</div>
+            <div class="pors__org">PQR</div>
             <div class="pors__desc">Led a 200-member club. Organised 12 events with 2000+ total attendance. Managed sponsorships and cross-college collaborations.</div>
           </div>
         </div>
@@ -41,8 +41,8 @@ function renderPors() {
           </div>
           <div class="pors__line"></div>
           <div class="pors__content">
-            <div class="pors__role">Vice President</div>
-            <div class="pors__org">Finance Club, IIM Udaipur</div>
+            <div class="pors__role">Y</div>
+            <div class="pors__org">CDE</div>
             <div class="pors__desc">Coordinated finance workshops and guest speaker sessions. Managed a team of 15 members.</div>
           </div>
         </div>
@@ -55,7 +55,7 @@ function renderPors() {
           <div class="pors__line"></div>
           <div class="pors__content">
             <div class="pors__role">Head</div>
-            <div class="pors__org">Cultural Committee</div>
+            <div class="pors__org">somewhere</div>
             <div class="pors__desc">Planned and executed the annual cultural fest with 500+ participants across 3 campuses.</div>
           </div>
         </div>
